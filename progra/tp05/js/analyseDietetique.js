b0a73ad7202f7be62d94a05d1870351abc8fb8e6
Diomenios <loui.arys@gmail.com>
2018-10-19 14:12:11 +0200

    ajout des commentaires à analyseDietetique.js + mise à jour

--- progra/tp05/js/analyseDietetique.js
+++ progra/tp05/js/analyseDietetique.js
@@ -79,12 +79,18 @@ function afficherNombreSemaines(){
 	console.log("Vous avez introduit des données pour " + nombreSemaines + " " + formatWord("semaine", nombreSemaines) + ".");
 }
 
+/*
+* Affiche en console le nombre de pizzas mangées par semaine.
+*/
 function afficherPizzasParSemaine(){
 	for(let i = 0 ; i<pizzas.length ; i++){
 		console.log("Lors de la semaine " + (i+1) + ", vous avez mangé " + pizzas[i] + " " + formatWord("pizza", pizzas[i]) + ".")
 	}
 }
 
+/*
+* Affiche en console le nombre maximum de pizzas mangées en une semaine.
+*/
 function afficherMaximumPizzas(){
 	let max = pizzas[0];
 
@@ -93,10 +99,12 @@ function afficherMaximumPizzas(){
 			max = pizzas[i];
 		}
 	}
-
 	console.log("Le plus grand nombre de pizzas mangées en une semaine est : " + max + ".");
 }
 
+/*
+* Affiche en console le nombre de semaine où au moins 3 pizzas ont été mangées;
+*/
 function afficherPlusDe3Pizzas(){
 	let numb = 0;
 	for(let i = 0 ; i < pizzas.length ; i++){
@@ -107,6 +115,13 @@ function afficherPlusDe3Pizzas(){
 	console.log("Vous avez mangé " + numb + " fois plus de " + LIMITE  + " " + formatWord("pizza", numb) + " durant la semaine.");
 }
 
+/*
+* Affiche en console le nombre de pizzas mangées durant un nombre de semaines dernières
+* rentré en paramètre.
+*
+* @param {number} nombreSemaines Le nombre de semaines durant lesquelles on veut
+* 															 compter les pizzas mangées.
+*/
 function afficherDernieresPizzas(nombreSemaines){
 	let acc = 0 ;
 	if(nombreSemaines < 1){
@@ -129,6 +144,11 @@ function afficherDernieresPizzas(nombreSemaines){
 				formatWord("semaine", nombreSemaines) +", vous avez mangé " + acc + " " + formatWord("pizza", acc) + ".");
 }
 
+/*
+* Compte le combre de pizzas total qui ont été enregistrée dans l'array pizzas.
+*
+* @return {number} acc Le nombre de pizzas total qui ont été enregistré.
+*/
 function compterTotalPizzas(){
 	let acc = 0;
 	for(let i = 0 ; i < pizzas.length ; i++){
@@ -137,21 +157,37 @@ function compterTotalPizzas(){
 	return acc;
 }
 
+/*
+* Affiche en console le poids des pizzas qui ont été mangées.
+* Utilise la fonction compterTotalPizzas() pour avoir le nombre total des pizzas mangées.
+*/
 function afficherPoidsPizzas(){
 	console.log("Au total sur les " + pizzas.length + " " + formatWord("dernière", pizzas.length) + " " + formatWord("semaine", pizzas.length) +
 				" vous avez mangé " + (compterTotalPizzas()*POIDS_PIZZA).toFixed(2) + " kg de pizzas.");
 }
 
+/*
+* Affiche en console le nombre moyen de pizzas mangées par semaine.
+* Utilise la fonction compterTotalPizzas() pour obtenir le nombre totale de pizzas.
+* Arrondis ce nombre au deux premières décimale.
+*/
 function afficherMoyennePizzas(){
 	if(pizzas.length == 0){
 		console.log("vous n'avez pas encore mangé de pizzas !!");
 	}
 	else{
-		let moyenne = compterTotalPizzas()/pizzas.length;
+		let moyenne = (compterTotalPizzas()/pizzas.length).toFixed(2);
 		console.log("En moyenne, vous avez mangé " + moyenne + " " + formatWord("pizza", moyenne) + " par semaine.");
 	}
 }
 
+/*
+* Affiche en console le plus grand nombre de pizzas mangées sur un
+* certains nombre de semaines consécutives.
+*
+* @param {number} nombreSemaines Le nombre de semaines consécutives durant lesquelles
+* 															 on veut faire le calcul.
+*/
 function afficherMaxPizzas(nombreSemaines){
 	if(nombreSemaines > pizzas.length || nombreSemaines < 1){
 		console.log("veuillez utiliser cette fonction avec un nombre valide (plus petit ou égal au nombre de semaines déjà introduites et plus grand que 0) !!");
